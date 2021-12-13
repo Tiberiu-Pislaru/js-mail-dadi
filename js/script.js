892dfbc2a3b28186a92406e33b6f1b8cee6ed653
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2021-12-13 13:36:16 +0100

    Comments

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1 chiediamo inizialmente la mail al nostro utente
+
+// 2 controlliamo che sia nella lista delle persone che possono accedere
+// 2.a creiamo una lista (array) con diverse mail al suo interno
+// 2.b creiamo una variabile 'flag' che inizialmente avrà valore false
+
+// 3 stampiamo un messaggio in base all'esito del controllo
